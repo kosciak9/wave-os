e1b1bef49ca220cedfb0155ce78ad030b53f2b18
Franciszek Madej <f.madej@protonmail.com>
2026-07-31 13:31:09 +0200

    feat(home): configure vicinae to a dot

--- modules/home/vicinae/default.nix
+++ modules/home/vicinae/default.nix
@@ -29,6 +29,9 @@ in
       theme = {
         dark = {
           name = "kanagawa";
+        }
+        // lib.optionalAttrs pkgs.stdenv.isLinux {
+          icon_theme = "oomox-Kanagawa";
         };
         light = {
           name = "kanagawa";
@@ -38,6 +41,65 @@ in
         compact_mode = {
           enabled = true;
         };
+        rounding = 8;
+        client_side_decorations = {
+          enabled = true;
+        };
+      };
+      close_on_focus_loss = true;
+      pop_to_root_on_close = true;
+      escape_key_behavior = "close_window";
+      providers = {
+        "browser-extension" = {
+          enabled = false;
+        };
+        core.entrypoints = {
+          about.enabled = false;
+          documentation.enabled = false;
+          keybind-settings.enabled = false;
+          list-extensions.enabled = false;
+          manage-fallback.enabled = false;
+          oauth-token-store.enabled = false;
+          open-config-file.enabled = false;
+          open-default-config.enabled = false;
+          prune-memory.enabled = false;
+          refresh-apps.enabled = false;
+          reload-scripts.enabled = false;
+          report-bug.enabled = false;
+          search-builtin-icons.enabled = false;
+          search-emojis.enabled = false;
+          show-logs.enabled = false;
+          sponsor.enabled = false;
+        };
+        developer.entrypoints.create.enabled = false;
+        font.entrypoints.browse.enabled = false;
+        "manage-shortcuts".enabled = false;
+        power.entrypoints = {
+          hibernate.enabled = false;
+          sleep.enabled = true;
+          soft-reboot.enabled = false;
+          suspend.enabled = false;
+        };
+        "raycast-compat" = {
+          enabled = false;
+          entrypoints.store.enabled = false;
+        };
+        scripts.enabled = false;
+        shortcuts.enabled = false;
+        snippets.enabled = false;
+        system = {
+          enabled = false;
+          entrypoints = {
+            run.enabled = false;
+            volume-down.enabled = false;
+            volume-up.enabled = false;
+          };
+        };
+        theme = {
+          enabled = true;
+          entrypoints.set.enabled = false;
+        };
+        wm.enabled = false;
       };
     };
     systemd = lib.mkIf pkgs.stdenv.isLinux {
