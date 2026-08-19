6ef674957395810404d736f5ff2f5e9a73d3afe1
Franciszek Madej <f.madej@protonmail.com>
2026-08-19 14:05:31 +0200

    chore(deps): bump devenv to 2.2.2

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    devenv-nixpkgs.url = "github:NixOS/nixpkgs/b266e7d2fc3562d97a8b5ed91cc914e8dac4a4c1";
+    devenv-nixpkgs.url = "github:NixOS/nixpkgs/0ae2bc1419c3f345984c2629e72e7a631820fa4d";
     # Vicinae intentionally keeps its release-tested Nixpkgs pin; following repository Nixpkgs triggers the known qtkeychain Darwin ld64 crash.
     vicinae = {
       url = "github:vicinaehq/vicinae/v0.25.0";
